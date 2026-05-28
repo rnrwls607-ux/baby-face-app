a8124d7a9b3489fef321f639e6f76c6058d23a19
rnrwls607-ux <rnrwls607@gmail.com>
2026-05-28 22:41:41 +0900

    next-pwa 제거

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 import type { NextConfig } from "next";
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-});
 
 const nextConfig: NextConfig = {};
 
-module.exports = withPWA(nextConfig);
+export default nextConfig;
